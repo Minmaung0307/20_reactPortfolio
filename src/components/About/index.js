bce7e53ad42d17b6522f4e49453ab0eed6aa1577
Min Maung <minmaung0307@gmail.com>
2022-10-24 17:26:41 -0400

    added about and cover img

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,7 +3,7 @@ import coverImage from "../../assets/cover/cover-image.jpg";
 function About() {
   return (
     <section className="my-5">
-      <h1 id="about">Portfolio</h1>
+      <h1 id="about">Beauty of People</h1>
       <img
         src={coverImage}
         className="my-2"
@@ -11,7 +11,16 @@ function About() {
         alt="cover"
       />
       <div className="my-2">
-        <p>Hello</p>
+        <p>
+          Padaung (Yan Pa Doung) is a Shan term for the <bold>Kayan Lahwi</bold>{" "}
+          (the group in which women wear the brass neck rings). The Kayan
+          residents in Mae Hong Son Province in Northern Thailand refer to
+          themselves as Kayan and object to being called Padaung. <br />
+          Myanmar: Shan State: 80,000 <br />
+          Myanmar: Kayah State: 70,000
+          <br />
+          Thailand: 600
+        </p>
       </div>
     </section>
   );
