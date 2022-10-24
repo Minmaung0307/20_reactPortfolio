2ceb59e49c784eb46fa2011a4cd986fcd11532d3
Min Maung <minmaung0307@gmail.com>
2022-10-24 14:06:00 -0400

    homepage about

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Landing from "./pages/Landing";
+import Navbar from "./components/Navbar";
+import AboutMe from "./pages/About";
 
 function App() {
+  const [page, setPage] = useState("About Me");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="scroll">
+      <Landing setPage={setPage} />
+      <Navbar setPage={setPage} page={page} />
+      <AboutMe setPage={setPage} />
+    </main>
   );
 }
 
---
+++ src/pages/Landing.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Footer from "../components/Footer";
+import "../styles/Landing.css";
+import logo from "../assets/images/512.png";
+import TypeAnimation from "react-type-animation";
+
+function Landing(props) {
+  const handlePageChange = (e) => {
+    props.setPage("About Me");
+  };
+
+  return (
+    <div
+      id="landing"
+      className="scroll-snap d-flex flex-column"
+      onMouseEnter={handlePageChange}
+    >
+      <div className="center">
+        <img src={logo} alt="avatar" className="photo mb-2" />
+
+        <TypeAnimation
+          cursor={true}
+          sequence={["", 500, "", 2000]}
+          wrapper="h1"
+          repeat={6}
+        />
+        <h5>Software Developer</h5>
+        <Footer />
+      </div>
+    </div>
+  );
+}
+
+export default Landing;
