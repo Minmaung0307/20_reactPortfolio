37b589c35a5e6964f5eb0064d61ab9bad01117e2
Min Maung <minmaung0307@gmail.com>
2022-10-25 18:47:00 -0400

    update some

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -37,6 +37,15 @@ function Nav(props) {
           <li className={`mx-2 ${contactSelected && "navActive"}`}>
             <span onClick={() => setContactSelected(true)}>Contact</span>
           </li>
+          <li className="mx-2">
+            <a
+              data-testid="resume"
+              href="#resume"
+              onClick={() => setContactSelected(false)}
+            >
+              Resume
+            </a>
+          </li>
           {categories.map((category) => (
             <li
               className={`mx-1 ${
