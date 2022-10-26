d7679ef46df966fd9b051020b3dfe32b10504db6
Min Maung <minmaung0307@gmail.com>
2022-10-26 15:27:53 -0400

    add resume and update some

---
+++ src/components/Resume/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { capitalizeFirstLetter } from "../../utils/helpers";
+
+function Resume() {
+  return (
+    <section className="my-5">
+      <h1 id="about">Resume</h1>
+      <div className="my-2">download</div>
+    </section>
+  );
+}
+
+export default Resume;
